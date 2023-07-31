daa60f30f52bd5ec83d03f96a48e49957365aab0
cs-ankit-gupta <gankit@fortinet.com>
2023-07-31 14:34:54 +0530

    New action to fetch KEV CVEs

--- cisa-advisory/constants.py
+++ cisa-advisory/constants.py
@@ -3,3 +3,4 @@ ADVISORY_TYPE = {
     "ICS Medical Advisory": "ics-medical-advisory"
 }
 CISA_FOLDER_PATH = '/connectors/data-point/cisa-advisory/'
+CISA_KNOWN_EXPLOITED_VULNERABILITY_URL = 'https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json'
